5dafffb58f3dd43b2e8b4193f01da1703842c703
jayzher juaniza <jayzher@classify.com.ph>
2025-01-09 09:46:21 +0800

    Commit Vite

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,10 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/classify-website' //Change to Repository Name
+  base: '/classify-website', //Change to Repository Name
+  build: {
+    chunkSizeWarningLimit: 20000, // Set chunk size warning limit to 1000 KB (1 MB)
+  },
 });
 
 // export default defineConfig({
